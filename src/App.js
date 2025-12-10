71e0e7bb324840cf4458157f38f493143a9b8245
shahil shrestha <shahilshrestha99@gmail.com>
2025-12-10 22:28:28 +0545

    Fix routing issues and switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import WatchPage from "./pages/WatchPage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/watch/:id" element={<WatchPage />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/watch/:id" element={<WatchPage />} />
+    </Routes>
   );
 }
 
